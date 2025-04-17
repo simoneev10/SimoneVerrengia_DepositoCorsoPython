6e6793db4c61294f66223f24736ee36af5376b3f
simoneev10 <simoneev1010@gmail.com>
2025-04-17 14:26:23 +0200

    Pending

---
+++ 17_04_CorsoPython/EsLinaspaceRandom.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+#La funzione linspace genera un array di numeri equidistanti tra un valore iniziale e uno finale
+arr = np.linspace(0,1,5)
+print(arr,"\n")
+
+# Serve per generare una matrice casuale 3x3
+random_arr = np.random.rand(3,3)
+print(random_arr)
